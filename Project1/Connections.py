33a964e4bff6c5f85ec877e95488936b44b99592
elayarajathiru92 <151925193+elayarajathiru92@users.noreply.github.com>
2023-12-09 19:00:32 +0530

    removed the credentials

--- Project1/Connections.py
+++ Project1/Connections.py
@@ -4,28 +4,31 @@ from googleapiclient.discovery import build
 import sqlalchemy
 
 # local MySQL connection details
+# replace your credentials here
 mydb = mysql.connector.connect(
-    host="localhost",
-    user="root",
+    host="",
+    user="",
     password="",
-    database="viswa"
+    database=""
 )
 my_sql_cursor = mydb.cursor(buffered=True)
 
-database_username = 'root'
+# replace your credentials here
+database_username = ''
 database_password = ''
-database_ip       = 'localhost'
-database_name     = 'viswa'
+database_ip       = ''
+database_name     = ''
 sql_connection = sqlalchemy.create_engine('mysql+mysqlconnector://{0}:{1}@{2}/{3}'.
                                                format(database_username, database_password, 
                                                       database_ip, database_name))
 
 #MongoDB Atlas config details and connections
-client = MongoClient("mongodb+srv://elayarajathiru:1234@cluster0.tklibaz.mongodb.net/?retryWrites=true&w=majority")
+# replace your credentials here
+client = MongoClient("")
 mongo_db = client.youtube_data
 collections=mongo_db.channels
 
 
 # Youtube API connections details
-api_key = "AIzaSyC0DiVDCzkqhscbbeGCIyzhgKBshXliPv4"
+api_key = "" # replace your api key credentials here
 youtube = build('youtube','v3',developerKey=api_key)
