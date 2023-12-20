31aa057d37121e08f1f6233dc29c19cf148ebfc4
elayarajathiru92 <151925193+elayarajathiru92@users.noreply.github.com>
2023-12-20 14:35:07 +0530

    Update YoutubeDataHarvesting_and_warehousing.py

--- Project1/YoutubeDataHarvesting_and_warehousing.py
+++ Project1/YoutubeDataHarvesting_and_warehousing.py
@@ -1,13 +1,13 @@
-from Connections import sql_connection,mongo_db,youtube,collections,my_sql_cursor
+from Connections import sql_connection,collections
 from YoutubeDataFetch import InsertMongoDBData
 import streamlit as st
 from streamlit_option_menu import option_menu
-from QuestionsDataFetch import questions,sql_q1,sql_q2,sql_q3,sql_q4,sql_q5,sql_q6,sql_q7,sql_q8,sql_q9,sql_q10
+from QuestionsDataFetch import questions,q1,q2,q3,q4,q5,q6,q7,q8,q9,q10
 import pandas as pd
 
 #creating sidebar menus
 with st.sidebar:
-    selected = option_menu("Main Menu",["Home","Extract & Transform","View"],
+    selected = option_menu("",["Home","Extract & Transform","View"],
                            icons=['house','database-fill-add','view-stacked'],menu_icon="cast",default_index=0)
 
 #Function to get existing channels from mongoDB
@@ -31,10 +31,11 @@ if selected == "Extract & Transform":
     with tab1:
         st.write("Enter the Youtube Channel id :")
         channel_ids = st.text_input("Hint : If providing more than one channels with ',' then provide next channel id.").split(',')
-        existsChannels=channel_names()
+
         for chn_id in channel_ids:
             if st.button("Upload to MongoDB"):
                 with st.spinner('Please Wait for it...'):
+                    existsChannels=channel_names()
                     if len(existsChannels)>= 0 and (chn_id in existsChannels.values()) == False:
                         chn=InsertMongoDBData(chn_id)
                         collections.insert_one(chn)
@@ -45,75 +46,63 @@ if selected == "Extract & Transform":
 
     with tab2:
         st.write("Select a channel to begin Transformation to SQL")
+        selected_channelId = 0
         existsChannels=channel_names()
-        if len(existsChannels)>0:
-            selected_channel = st.selectbox('',options = list(existsChannels.keys()))
-            selected_channelId = existsChannels[selected_channel]
-        else:
-            selected_channel = st.selectbox('',options = "", index=None,placeholder="Select channel...")
-            selected_channelId = 0
+        
+        selected_channel = st.selectbox('Channels',options = list(existsChannels.keys()), index=None,placeholder="Select channel...")
+
+        selected_channelId = existsChannels[selected_channel] if selected_channel !=None else 0
         Migrate = st.button('**Migrate to MySQL**')
-
-        try:
-            my_sql_cursor.execute("SELECT Channel_id FROM channels")
-            myresult = my_sql_cursor.fetchall()
-        except:
-            myresult=[]
-
-        mysql_channelIds=[]
-        for i in myresult:
-            mysql_channelIds.append(i[0])
-        my_sql_cursor.close()
-
         
         if Migrate:
             with st.spinner('Please Wait for it...'):
-                if len(mysql_channelIds)>=0 and (selected_channelId not in mysql_channelIds):
-                    # Retrieve the document with the specified name
-                    result = collections.find_one({"_id": selected_channelId})
-                    # Data Conversion
-                    # channel details conversion
-                    channel_df = pd.DataFrame.from_dict(result["channel_detail"][0], orient='index').T
-                    channel_df.to_sql(con=sql_connection,name='channels', if_exists='append',index=False)
+                # Retrieve the document with the specified name
+                result = collections.find_one({"_id": selected_channelId})
+                # Data Conversion
+                # channel details conversion
+                channel_df = pd.DataFrame.from_dict(result["channel_detail"][0], orient='index').T
+                channel_df.to_sql(con=sql_connection,name='channels', if_exists='append',index=False,chunksize=50)
 
-                    # vedio details conversion
-                    vedios_df = pd.DataFrame(result["vedio_detail"])
-                    vedios_df.to_sql(con=sql_connection,name='vedios', if_exists='append',index=False)
+                # vedio details conversion
+                vedios_df = pd.DataFrame(result["vedio_detail"])
+                vedios_df.to_sql(con=sql_connection,name='vedios', if_exists='append',index=False,chunksize=50)
 
-                    # comment details conversion
-                    Comments_df = pd.DataFrame(result["comment_detail"])
-                    Comments_df.to_sql(con=sql_connection,name='comments', if_exists='append',index=False)
+                # comment details conversion
+                Comments_df = pd.DataFrame(result["comment_detail"])
+                Comments_df.to_sql(con=sql_connection,name='comments', if_exists='append',index=False,chunksize=50)
 
-                    st.success("Transfered to MySQL successful !!")
-                else:
-                    st.warning(f"Already for this channel - {selected_channel} - data is transfered to MySQL")
+                st.success("Transfered to MySQL successful !!")
                 
     
 # View page data
 if selected == "View":
+    selected_QuesId = 0
     st.write("## :orange[Select any question to get Insights]")
-    selected_Ques = st.selectbox('**Select Any Question', options = list(questions.values()), key='question_names')
-    for key, val in questions.items():
-        if val == selected_Ques:
-            selected_QuesId=key
+    selected_Ques = st.selectbox('**Select Any Question', options = list(questions.values()), index=None,placeholder="Select Question...")
+    if selected_Ques != None:
+        for key, val in questions.items():
+            if val == selected_Ques:
+                selected_QuesId=key
     
     if selected_QuesId == 1:
-        st.dataframe(sql_q1(),hide_index=True)
+        st.dataframe(q1(),hide_index=True)
     elif selected_QuesId == 2:
-        st.dataframe(sql_q2(),hide_index=True)
+        st.dataframe(q2(),hide_index=True)
     elif selected_QuesId == 3:
-        st.dataframe(sql_q3(),hide_index=True)
+        st.dataframe(q3(),hide_index=True)
     elif selected_QuesId == 4:
-        st.dataframe(sql_q4(),hide_index=True)
+        st.dataframe(q4(),hide_index=True)
     elif selected_QuesId == 5:
-        st.dataframe(sql_q5(),hide_index=True)
+        st.dataframe(q5(),hide_index=True)
     elif selected_QuesId == 6:
-        st.dataframe(sql_q6(),hide_index=True)
+        st.dataframe(q6(),hide_index=True)
     elif selected_QuesId == 7:
-        st.dataframe(sql_q7(),hide_index=True)
+        st.dataframe(q7(),hide_index=True)
     elif selected_QuesId == 8:
-        st.dataframe(sql_q8(),hide_index=True)
+        st.dataframe(q8(),hide_index=True)
     elif selected_QuesId == 9:
-        st.dataframe(sql_q9(),hide_index=True)
+        st.dataframe(q9(),hide_index=True)
     elif selected_QuesId == 10:
-        st.dataframe(sql_q10(),hide_index=True)
+        st.dataframe(q10(),hide_index=True)
+    else:
+        pass
